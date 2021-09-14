f6aca61dc6952c13176101d78a7b7e41ca9770fb
TeoTheTorch <57500649+TheoTheTorch@users.noreply.github.com>
2021-09-14 20:45:09 +0200

    Everything is good rn. Not the enemies yet.

--- Enemies/EnemyHitbox.gd
+++ Enemies/EnemyHitbox.gd
@@ -2,5 +2,6 @@ extends Area2D
 
 
 
-func _on_EnemyHitbox_body_entered(body: Node) -> void:
-	get_tree().call_group("Player", "collided_with_enemy", get_parent().damage)
+
+func _on_EnemyHitbox_area_entered(area: Area2D) -> void:
+	print("hurt enemy")
---
+++ WeaponSystem/WeaponSystem.gd
@@ -0,0 +1,12 @@
+extends Position2D
+
+export var move_time : float = 0.2
+export var node_path : NodePath
+
+func _physics_process(delta: float) -> void:
+	move()
+
+func move():
+	$TweenPos.interpolate_property(self, "position", position, get_node(node_path).global_position,
+			move_time, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	$TweenPos.start()
